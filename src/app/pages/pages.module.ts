5b913141e75f735709ded3c1ad33dfc85a9bf716
cosentinowebdev <rodrigojcosentino@gmail.com>
2023-07-19 01:43:22 -0300

    chore

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
 
+import { SharedModule } from '../shared/shared.module';
 
 import { RouterOutlet } from '@angular/router';
 
@@ -29,7 +31,8 @@ import { RouterOutlet } from '@angular/router';
   imports: [
     CommonModule,
     SharedModule,
-    RouterOutlet
+    RouterOutlet,
+    FormsModule
     // AppRoutingModule
   ]
 })
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -2,8 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
-  templateUrl: './progress.component.html'
+  templateUrl: './progress.component.html',
+  styleUrls:["./progress.component.css"]
 })
 export class ProgressComponent {
+
+  progreso:number= 50;
+  get getPorcentaje(){
+    return `${this.progreso}%`;
+  }
+  cambiarValor(valor:number){
+    if (this.progreso >= 100 && valor >= 0) {
+      this.progreso = 100;
+    }
+    if (this.progreso <= 0 && valor <= 0) {
+      this.progreso = 0;
+    }
+    this.progreso = this.progreso + valor;
+  }
 
 }
